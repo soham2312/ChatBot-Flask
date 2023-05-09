231174ec0f252b9572671c5e98a3244fbb457565
Soham <sohamsarode2312@gmail.com>
2023-05-09 11:34:58 +0530

    Changes done

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
 python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+python3 manage.py runserver
 echo " BUILD END" 
 
 
--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f!*1qm$2@nj%m(%dalbv^ie)a!=70n#igsoizhd322v7(y#zf!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
 
 # Application definition
